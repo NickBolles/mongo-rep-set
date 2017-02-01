4fd89bde91db6c8688eabfc7d3285e3243417ef1
Nick Bolles <me@nickbolles.com>
2017-02-01 10:04:26 -0600

    Update mongo_setup_repset.sh

--- scripts/mongo_setup_repset.sh
+++ scripts/mongo_setup_repset.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
+if [ "$MONGO_AUTH" == true ]; then
+  echo "************************************************************"
+  echo "Setting up replica set"
+  echo "************************************************************"
 
-echo "************************************************************"
-echo "Setting up replica set"
-echo "************************************************************"
-
-gosu mongodb mongo admin --eval "help" > /dev/null 2>&1
-RET=$?
-
-while [[ RET -ne 0 ]]; do
-  echo "Waiting for MongoDB to start..."
   gosu mongodb mongo admin --eval "help" > /dev/null 2>&1
   RET=$?
-  sleep 1
 
-  if [[ -f /data/db/mongod.lock ]]; then
-    echo "Removing Mongo lock file"
-    rm /data/db/mongod.lock
-  fi
-done
+  while [[ RET -ne 0 ]]; do
+    echo "Waiting for MongoDB to start..."
+    gosu mongodb mongo admin --eval "help" > /dev/null 2>&1
+    RET=$?
+    sleep 1
 
-# Login as root and configure replica set
-gosu mongodb mongo admin -u $MONGO_ROOT_USER -p $MONGO_ROOT_PASSWORD --eval "rs.initiate();"
-gosu mongodb mongo admin -u $MONGO_ROOT_USER -p $MONGO_ROOT_PASSWORD --eval "rs.add('$MONGO_SECONDARY');"
-gosu mongodb mongo admin -u $MONGO_ROOT_USER -p $MONGO_ROOT_PASSWORD --eval "rs.addArb('$MONGO_ARBITER');"
+    if [[ -f /data/db/mongod.lock ]]; then
+      echo "Removing Mongo lock file"
+      rm /data/db/mongod.lock
+    fi
+  done
+
+  # Login as root and configure replica set
+  gosu mongodb mongo admin -u $MONGO_ROOT_USER -p $MONGO_ROOT_PASSWORD --eval "rs.initiate();"
+  gosu mongodb mongo admin -u $MONGO_ROOT_USER -p $MONGO_ROOT_PASSWORD --eval "rs.add('$MONGO_SECONDARY');"
+  gosu mongodb mongo admin -u $MONGO_ROOT_USER -p $MONGO_ROOT_PASSWORD --eval "rs.addArb('$MONGO_ARBITER');"
+fi
